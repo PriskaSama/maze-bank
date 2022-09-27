f7a6e8bcd49463803a2041f6eea086eacee770d7
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2022-09-27 15:25:51 +0200

    double checked netlify

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-
 module.exports = {
   entry: path.resolve(__dirname, "./src/app.js"),
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./dist"),
     clean: true,
   },
   optimization: {
@@ -15,33 +14,33 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/pages/home/index.html"),
-      filename: "index.html",
-      title: 'Production',
-    }),
+        template: path.resolve(__dirname, "./src/index.html"),
+        filename: "index.html",
+        title: 'Production',
+      }),
     new CopyPlugin({
       patterns: [
-        { from: "./src/assets/fonts/", to: "assets/fonts" },
-        { from: "./src/assets/img", to: "assets/img" },
+        { from: "./src/assets/fonts/", to: "assets/fonts/" },
+        { from: "./src/assets/img/", to: "assets/img/" },
       ],
     }),
   ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
       {
-        test: /\.m?js$/,
+        test: /.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
