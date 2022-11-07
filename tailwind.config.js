9cfd38abca3364a30a1635085596ae8d8d9bea24
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2022-11-07 11:23:51 +0100

    updated bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
-const { slate } = require('tailwindcss/colors');
+const { slate } = require("tailwindcss/colors");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      'neonblue': '#65cbe9',
-      white: '#ffffff',
+      neonblue: "#65cbe9",
+      white: "#ffffff",
       slate: colors.slate,
       gray: colors.gray,
       zinc: colors.zinc,
@@ -32,16 +32,15 @@ module.exports = {
       pink: colors.pink,
       rose: colors.rose,
     },
-    extend: {
-    },
-    
+    extend: {},
+
     backgroundImage: {
-        "moon": "https://img.freepik.com/free-vector/vector-vibrant-neon-circle-with-glow_87202-1037.jpg?w=1380&t=st=1667815388~exp=1667815988~hmac=19eac25380491733b2ffb3f5a800f7eebb5bcc725bd2fd61b8074c0f2f9b625d')",
-      },
+      "moon": "url('/src/assets/img/abstractpurple-bg.jpg)",
+    },
 
     fontFamily: {
-        'roboto': ['/src/assets/fonts/Roboto/Roboto-Medium.ttf'],
-      },
+      roboto: ["/src/assets/fonts/Roboto/Roboto-Medium.ttf"],
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
